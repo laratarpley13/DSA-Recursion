56d22f08ac192f33e7b9edd021658835a5ff2b4f
laratarpley13 <ltarp13@gmail.com>
2021-03-26 14:31:00 -0500

    completed counting sheep problem

--- counting-sheep.js
+++ counting-sheep.js
@@ -1,1 +1,18 @@
-console.log("test")
+//write program that counts how many sheep jump over fence
+//takes in a number as input (should be number of sheep you have)
+/*
+Function should display number with message "Another sheep jumps 
+over the fence" until no sheep are left
+*/
+
+const countSheep = function(numOfSheep) {
+    //Base Case
+    if (numOfSheep === 0) {
+        return "All sheep jumped over the fence"
+    }
+    // General case
+    console.log(`${numOfSheep}: Another sheep jumps over the fence`);
+    return countSheep(numOfSheep - 1)
+}
+
+console.log(countSheep(6));
